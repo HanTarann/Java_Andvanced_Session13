5dd81f331db70e8414b9a0a8d23c16d344a060d3
TranKhanhAn <trankhanhan080506@gmail.com>
2026-03-27 08:27:25 +0700

    session13

--- src/util/DBConnection.java
+++ src/util/DBConnection.java
@@ -4,7 +4,7 @@ public class DBConnection {
     public static java.sql.Connection getConnection() throws java.sql.SQLException {
         String url = "jdbc:mysql://localhost:3306/SS13";
         String username = "root";
-        String password = "anlinh.99";
+        String password = "trankhanh8506";
         return java.sql.DriverManager.getConnection(url, username, password);
     }
 }
